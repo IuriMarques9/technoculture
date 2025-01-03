35f0adc91a0d93d8e66ebb44df2b679ecdece2e1
Iuri Marques <iurimarcianom@hotmail.com>
2025-01-03 13:50:49 +0000

    ee

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,29 @@ import Link from "next/link";
 export default function Footer(){
     
 
-    return (
-        <footer className="bg-gradient-to-b from-lightRed from-90 to-darkRed to-100 px-4 flex flex-col gap-2">
-          <section>
-            <h2>Newsletter</h2>
-            
-            <p>Subscribe to our newsletter to stay up to date with the latest events and discounts.</p>
+  return (
+    <footer className="bg-gradient-to-b from-lightRed from-90 to-darkRed to-100 px-4 flex flex-col gap-2">
+      <section>
+        <h2>Newsletter</h2>
+          
+        <p>Subscribe to our newsletter to stay up to date with the latest events and discounts.</p>
 
-            ADD FORM
+        ADD FORM
 
-            <p className="text-sm">By clicking Subscribe, you agree with our <a>Terms and Conditions</a>.</p>
-          </section>
+        <p className="text-sm">By clicking Subscribe, you agree with our <a>Terms and Conditions</a>.</p>
 
-          <section>
-            <div className="flex justify-around py-2">
-              <Link href="" className="hover:underline text-sm">Return Policy</Link>
+      </section>
 
-              <Link href="" className="hover:underline text-sm">Privacy Policy</Link>
-            </div>
+      <section>
+        <div className="flex justify-around py-2">
+          <Link href="" className="hover:underline text-sm">Return Policy</Link>
+          <Link href="" className="hover:underline text-sm">Privacy Policy</Link>
+        </div>
 
-            <hr />
+        <hr />
 
-            <p className="text-center text-xs py-2">&#169; 2025 By TechnoCulture Portugal</p>
-          </section>
-        </footer>
-    );
+        <p className="text-center text-xs py-2">&#169; 2025 By TechnoCulture Portugal</p>
+      </section>
+    </footer>
+  );
 }
