4d52f9a9a3310c0e0c65d62b0a5a852aee567a99
Iuri Marques <iurimarcianom@hotmail.com>
2025-01-16 21:14:48 +0000

    `Removed unused imports from Caroussel.tsx`

--- src/components/Caroussel.tsx
+++ src/components/Caroussel.tsx
@@ -5,8 +5,6 @@ import "slick-carousel/slick/slick-theme.css";
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import Loader from "./LoadingPage";
-import { ChildProps } from "postcss";
 
 
 interface Events {
