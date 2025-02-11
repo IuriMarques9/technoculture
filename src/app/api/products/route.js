cbc2223914166872be1b260150f3c99d689fe1d8
Iuri Marques <iurimarcianom@hotmail.com>
2025-02-11 21:34:46 +0000

    `Refactor API route and database connection logic`

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,17 +1,16 @@
 import connectMongoDB from '../../../lib/dbConnect';
-import { NextResponse } from 'next/server';
 import Products from '../../../Models/Products';
 
 export async function GET() {
     try{
         await connectMongoDB();
 
-        //Find all events
+        //Find all products
         const data = await Products.find();
 
-        return NextResponse.json(data);
+        return Response.json(data);
     }catch (error) {
         console.error('Erro ao buscar eventos:', error);
-        return NextResponse.json({ message: 'Erro ao buscar eventos' }, { status: 500 });
+        return Response.json({ message: 'Erro ao buscar eventos' }, { status: 500 });
     }
 }
--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,14 +1,31 @@
 import mongoose from "mongoose";
 
-const connectMongoDB = () => {
-  
-  try{
-    mongoose.connect(process.env.MONGODB_URI)
-    console.log("Connected to MongoDB");
-  } catch(error){
-    console.log(error);
-  }
+const MONGODB_URI = process.env.MONGODB_URI;
 
+if (!MONGODB_URI) {
+  throw new Error("A variável de ambiente MONGODB_URI não está definida!");
 }
 
-export default connectMongoDB;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
